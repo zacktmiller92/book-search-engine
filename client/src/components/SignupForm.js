55284e8d43d855d071165d003f1e9152d0959d4e
zacktmiller92 <zacktmiller92@gmail.com>
2021-11-13 12:10:48 -0500

    replaced REST logic for adding users with mutation for adding users in SignupForm.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+import { useMutation, useQuery } from '@apollo/react-hooks';
+
+import { ADD_USER } from '../utils/mutations';
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +34,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      // const response = await createUser(userFormData);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await addUser({
+        variables: { ...userFormData }
+      });
+      console.log(data)
 
-      const { token, user } = await response.json();
-      // console.log(user);
-      Auth.login(token);
+      Auth.login(data.addUser.token)
+
     } catch (err) {
       console.error(err);
       setShowAlert(true);
